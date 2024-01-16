8daae82cfab8048e01e2f18c2a578ac1efac30be
Sarah Cavanaugh <sarahcavanaugh20@gmail.com>
2024-01-16 14:56:59 -0600

    quiz updates

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+var quizContainer = document.getElementById('quizQA');
+var submitContainer = document.getElementById('submitBtn');
+var resultsContainer = document.getElementById('results');
+var quiz1 = document.createElement('h1'); //quiz question 1?????
 var timeRemaning = 75; //75 seconds
 var startQuiz = document.getElementById('startBtn');
+console.log(startQuiz);
+var div = document.createElement('div');
+var answer1 = document.createElement('button');
+var answer2 = document.createElement('button');
+var answer3 = document.createElement('button');
 
 function displayTime() {
-	var div = document.createElement('div');
-	document.body.innerHTML = ''; // clear line each time
-	document.body.appendChild(div); //putting on the page
+	div.innerHTML = '';
 	div.innerHTML = timeRemaning; //the text (innerHTML) is showing the time remaining on the screen
+	quizContainer.appendChild(div);
 }
 
 function startCountdown() {
@@ -18,14 +26,67 @@ function startCountdown() {
 	}, 1000); // countdown by 1 millisecond intervals
 }
 
-startQuiz.addEventListener('click', function () {
-	startCountdown();
-});
-
-var quiz1 = document.createElement('h1');
 function showQuestions() {
 	quiz1.textContent = 'This is my first question';
 	quiz1.setAttribute = ('style', 'font-size: 200px');
-	document.body.appendChild(quiz1);
+	quizContainer.appendChild(quiz1); //appending it to container instead of body
+	answer1.textContent = 'this is answer a';
+	answer2.textContent = 'this is answer b';
+	answer3.textContent = 'this is answer c';
+	quizContainer.appendChild(answer1);
+	quizContainer.appendChild(answer2);
+	quizContainer.appendChild(answer3);
 }
-startQuiz.addEventListener('click', showQuestions);
+//
+
+function showResults1(event) {
+	console.log(event);
+	if (event.target.innerHTML === 'this is answer a') {
+		console.log('correct answer');
+	} else {
+		console.log('incorrect');
+	}
+}
+answer1.addEventListener('click', showResults1);
+answer2.addEventListener('click', showResults1);
+answer3.addEventListener('click', showResults1);
+
+//
+// var myQuestions = [
+// 	{
+// 		question1: 'This is my first Question',
+// 		answers1: {
+// 			a: 'answer1',
+// 			b: 'answer2',
+// 			c: 'answer3',
+// 		},
+// 		correctAnswer: 'a',
+// 	},
+// 	{
+// 		question2: 'This is my second question',
+// 		answers2: {
+// 			a: 'answer1',
+// 			b: 'answer2',
+// 			c: 'answer3',
+// 		},
+// 		correctAnswer: 'b',
+// 	},
+// 	{
+// 		question3: 'This is my third question',
+// 		answers3: {
+// 			a: 'answer1',
+// 			b: 'answer2',
+// 			c: 'answer3',
+// 		},
+// 		correctAnswer: 'c',
+// 	},
+// ];
+
+function clickQuiz() {
+	startCountdown();
+	showQuestions();
+}
+
+startQuiz.addEventListener('click', clickQuiz); //want to get it to display question continuously along with timer....?
+
+//function btnClick(){} ????
